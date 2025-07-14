b148b687e0ceb282979e44eeea0d149ef9aee8cc
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-07-14 12:21:54 +0200

    yachip-8: instruction fetch decode
    beginning of the cycle implementation.
    fetch decode and execute instruction
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use rand::prelude::*;
+use std::usize;
+
+// use rand::prelude::*;
 
 const FONTSET_SIZE: usize = 80;
 
@@ -21,33 +23,33 @@ pub const FONTSET: [u8; FONTSET_SIZE] = [
     0xF0, 0x80, 0xF0, 0x80, 0x80, // F
 ];
 
-struct CHIP8 {
+pub struct CHIP8 {
     ram: [u8; 0x1000],
     opcode: u16,
     graphics: [u8; 64 * 32],
     regs: [u8; 16],
     pc: u16,
-    delay_timer: u8,
-    sound_timer: u8,
-    index: u16,
+    // delay_timer: u8,
+    // sound_timer: u8,
+    // index: u16,
     sp: usize,
     stack : [u16; 16],
-    keys: [u8; 16],
+    // keys: [u8; 16],
 }
 
 impl CHIP8 {
-    fn new() -> CHIP8 {
+    pub fn new() -> CHIP8 {
         let mut chip = CHIP8 { 
             ram: [0; 0x1000],
             graphics: [0; 64 * 32],
             stack: [0; 16],
-            keys: [0; 16],
+            // keys: [0; 16],
             regs: [0; 16],
 
             opcode: 0,
-            delay_timer: 0,
-            sound_timer: 0,
-            index: 0,
+            // delay_timer: 0,
+            // sound_timer: 0,
+            // index: 0,
             sp: 0,
             pc: 0x200,
         };
@@ -57,8 +59,49 @@ impl CHIP8 {
         chip
     }
 
-    fn load_rom(&mut self, rom: &[u8]) {
+    pub fn load_rom(&mut self, rom: &[u8]) {
         self.ram[0..rom.len()].copy_from_slice(rom);  
+    }
+
+    pub fn increment_pc(&mut self) {
+        self.pc += 2;
+    }
+
+    pub fn cycle(&mut self) {
+        let byte1 = self.ram[self.pc as usize] as u16;
+        let byte2 = self.ram[self.pc as usize + 1] as u16;
+        self.opcode = (byte1 << 8) | byte2;
+
+        let ins: u8 = (byte1 as u8) >> 4;
+        match ins {
+            0x0 => {
+                match self.opcode {
+                    0x00E0 => self.graphics = [0; 64 * 32],
+                    0x00EE => { // Return
+                        self.sp -= 1;
+                        self.pc = self.stack[self.sp];
+                    }
+                    _ => panic!("Undefined opcode\n")
+                }
+                self.increment_pc();
+            },
+            0x1 => self.pc = self.opcode & 0xFFF,
+            0x2 => { // Call instruction
+                self.stack[self.sp] = self.pc;
+                self.sp += 1;
+                self.pc = self.opcode & 0x0FFF;
+            }
+            0x3 => {
+                let reg: u8 = (byte1 as u8) & 0x0F;
+                if self.regs[reg as usize] == (byte2 as u8) {
+                    self.increment_pc();
+                }
+                self.increment_pc();
+            }
+            _ => {
+                panic!("Undefined instruction\n");
+            },
+        }
     }
 }
 
@@ -80,5 +123,13 @@ mod tests {
         for (idx, _) in FONTSET.iter().enumerate() {
             assert_eq!(result.ram[idx], FONTSET[idx]);
         }
+    }
+
+    #[test]
+    fn clear_screen() {
+        let mut result = CHIP8::new();
+        result.ram[result.pc as usize] = 0x00;
+        result.ram[result.pc as usize + 1] = 0xE0;
+        result.cycle();
     }
 }
