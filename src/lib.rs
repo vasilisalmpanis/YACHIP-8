f0becb58c0652aa830faa3b325d86ee9beeb3b42
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-07-16 14:43:43 +0200

    yachip-8: more instructions.
    Missing D-F
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::usize;
 
-// use rand::prelude::*;
+use rand::prelude::Rng;
 
 const FONTSET_SIZE: usize = 80;
 
@@ -31,7 +31,7 @@ pub struct CHIP8 {
     pc: u16,
     // delay_timer: u8,
     // sound_timer: u8,
-    // index: u16,
+    index: u16,
     sp: usize,
     stack : [u16; 16],
     // keys: [u8; 16],
@@ -49,7 +49,7 @@ impl CHIP8 {
             opcode: 0,
             // delay_timer: 0,
             // sound_timer: 0,
-            // index: 0,
+            index: 0,
             sp: 0,
             pc: 0x200,
         };
@@ -117,6 +117,73 @@ impl CHIP8 {
                 let value: u8 = byte2 as u8;
                 let reg: u8 = (byte1 & 0x0F) as u8;
                 self.regs[reg as usize] = value;
+                self.increment_pc();
+            }
+            0x7 => {
+                let reg: u8 = byte1 as u8 & 0xF;
+                let value = self.regs[reg as usize].wrapping_add(byte2 as u8);
+                self.regs[reg as usize] = value;
+                self.increment_pc();
+            },
+            0x8 => {
+                let x = ((byte1 as u8) & 0x0F) as usize;
+                let y = ((byte2 as u8) & 0xF0 >> 4) as usize;
+                let m = (byte2 & 0x0F) as u8;
+                match m {
+                0x0 => {self.regs[x as usize] = self.regs[y as usize]},
+                0x1 => {self.regs[x as usize] |= self.regs[y as usize]},
+                0x2 => {self.regs[x as usize] &= self.regs[y as usize]},
+                0x3 => {self.regs[x as usize] ^= self.regs[y as usize]},
+
+                0x4 => {
+                    let mut sum: u16 = self.regs[x] as u16;
+                    sum = sum.wrapping_add(self.regs[y] as u16);
+                    self.regs[0xF] = if sum > 255 {1} else {0};
+                    self.regs[x] = sum as u8;
+                },
+                0x5 => {
+                    self.regs[0xF] = if self.regs[x] > self.regs[y] {1} else {0};
+                    self.regs[x] = self.regs[x].wrapping_sub(self.regs[y]);
+                },
+                0x6 => {
+                    self.regs[0xF] = self.regs[x] & 1; // LSB
+                    self.regs[x] >>= 1;
+                },
+                0x7 => {
+                    self.regs[0xF] = if self.regs[y] > self.regs[x] {1} else {0};
+                    self.regs[x] = self.regs[y].wrapping_sub(self.regs[x]);
+                },
+                0xe => {
+                    self.regs[0xF] = if self.regs[x] & 0x80 != 0 {1} else {0}; // MSB
+                    self.regs[x] <<= 1;
+                },
+                _=> {},
+                }
+                self.increment_pc();
+            },
+            0x9 => {
+                let reg1: u8 = (byte1 as u8) & 0x0F;
+                let reg2: u8 = ((byte2 as u8) & 0x0F) >> 4;
+                if self.regs[reg1 as usize] != self.regs[reg2 as usize] {
+                    self.increment_pc();
+                }
+                self.increment_pc();
+            }
+            0xA => {
+                self.index = self.opcode & 0x0FFF;
+                self.increment_pc();
+            }
+            0xB => {
+                self.index = self.opcode & 0x0FFF;
+                self.pc = self.opcode & 0x0FFF + (self.regs[0] as u16);
+                self.increment_pc();
+            }
+            0xC => {
+                let mut rng = rand::rng();
+                let byte: u8 = rng.random::<u8>();
+                let x = ((byte1 as u8) & 0x0F) as usize;
+                let result = byte & (byte2 as u8);
+                self.regs[x] = result;
                 self.increment_pc();
             }
             _ => {
